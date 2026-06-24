a7c0da847b4b01662323997e4e835c76198d9446
chandan kumar <iotoos.chandan@gmail.com>
2026-06-24 17:06:33 +0530

    chn

--- js/index.js
+++ js/index.js
@@ -2,35 +2,6 @@
  
  
 
-const cards = document.querySelectorAll(".service-card");
-const modal = document.getElementById("popupModal");
-
-const popupTitle = document.getElementById("popupTitle");
-const popupContent = document.getElementById("popupContent");
-const popupImg = document.getElementById("popupImg");
-
-cards.forEach(card=>{
-
-    card.addEventListener("click",()=>{
-
-        const title =
-        card.querySelector("h2,h3").innerText;
-
-        const content =
-        card.querySelector("p").innerText;
-
-        const image =
-        card.querySelector("img").src;
-
-        popupTitle.innerText = title;
-        popupContent.innerText = content;
-        popupImg.src = image;
-
-        modal.classList.add("active");
-
-    });
-
-});
 
 document.querySelector(".close-btn")
 .addEventListener("click",()=>{
